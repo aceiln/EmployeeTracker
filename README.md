5910917a4aad0fe8666dcbbfd891f4c75966abe4
Ella <ellathitotran@gmail.com>
2023-09-11 23:32:46 -0500

    added updated employee section

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# EmployeeTracker
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# Employee Tracker
+## Description
+The motivation of this project was to learn how to make interactive databases that the user can change, add, and view  values and information of.
+## Table of Contents
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+## Installation
+In order to develop this, you have to go into the .env file and enter your root name after installation, install npm -i. and make sure that you have mysql2. 
+## Usage
+To use it, you have to enable it by typing node server.js into your terminal and follow the upcoming prompts given to you.
+
+## License
+MIT
+MIT License
+
+Copyright (c) 2023 acein
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+## Questions? Contact me:
+**Github:** [aceiln](https://github.com/acein)
+
+**Email me:** notarealemail@gmail.com
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const mysql = require('mysql2');
 const db = require('./connection');
 
 db.connect((err) => {
-    if(err){
-      console.log(err);
+    if (err) {
+        console.log(err);
     }
     console.log('MySql Connected');
     start()
-  });
+});
 
 function start() {
     inquirer.prompt({
@@ -17,8 +17,8 @@ function start() {
         message: 'Select an option.',
         choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee']
     })
-    
-.then(handleUserResponse);
+
+        .then(handleUserResponse);
 };
 
 function handleUserResponse(answer) {
@@ -26,9 +26,9 @@ function handleUserResponse(answer) {
         case 'View all departments':
             return viewAll('department');
         case 'View all roles':
-            return viewAll('roles');
+            return viewAll('role');
         case 'View all employees':
-            return viewAll('employees');
+            return viewAll('employee');
         case 'Add a department':
             return addDepartment();
         case 'Add a role':
@@ -49,12 +49,107 @@ function viewAll(table) {
     })
 }
 
-function addDepartment(){
+function addDepartment() {
     inquirer.prompt({
-            type: 'input',
-            message: 'Department name: ',
-            name: 'departmentName'
+        type: 'input',
+        message: 'Department name: ',
+        name: 'departmentName'
+    })
+        .then(answer => {
+            db.query(`INSERT INTO department (name) VALUES ("${answer.departmentName}");`)
+            console.log('wow what a nerdy department name');
+            start()
         })
-    .then(answer => {
-        db.query(`INSERT INTO department (name) VALUES ("${answer.departmentName}");`)
-    })};
+};
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Employee first name: ',
+            name: 'first_name'
+        },
+        {
+            type: 'input',
+            message: 'Employee last name: ',
+            name: 'last_name'
+        },
+        {
+            type: 'input',
+            message: 'Role ID: ',
+            name: 'role_id'
+        },
+        {
+            type: 'input',
+            message: 'Manager ID: ',
+            name: 'manager_id'
+        }
+    ])
+        .then(answer => {
+            db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES ("${answer.first_name}","${answer.last_name}","${answer.role_id}","${answer.manager_id}");`)
+            console.log('wow what a nerd');
+            start()
+        })
+};
+
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Role name: ',
+            name: 'title'
+        },
+        {
+            type: 'input',
+            message: 'Salary total: ',
+            name: 'salary'
+        },
+        {
+            type: 'input',
+            message: 'Department ID: ',
+            name: 'department_id'
+        }
+    ])
+        .then(answer => {
+            db.query(`INSERT INTO role (title, salary, department_id)
+        VALUES ("${answer.title}","${answer.salary}","${answer.department_id}");`)
+            console.log('wow what a nerd');
+            start()
+        })
+};
+
+async function updateEmployee() {
+    const query = `SELECT * FROM employee`;
+
+    p = []
+
+    console.log(db.query(`SELECT * FROM role`, (err, role) => {
+        p = role
+    }));
+
+    db.query(query, (err, employee) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'employee',
+                message: 'Which employees role do you want to update?',
+                choices: employee.map(employee => employee.last_name)
+            },
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is their new role id?',
+                choices: p.map(r => r.id)
+            }
+        ])
+            .then(answer => {
+                const query = `UPDATE employee SET role_id = ${answer.role} WHERE employee.last_name = ${answer.employee}`;
+                db.query(query, (err, result) => {
+                    start();
+                });
+            });
+    });
+}
